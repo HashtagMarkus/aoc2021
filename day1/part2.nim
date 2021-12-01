5367ec5dfd9ce856df7ec6dd7432a69f49c7ae4a
Markus Hoefer <markus.hoefer@codecentric.de>
2021-12-01 14:51:19 +0100

    Implement part2

---
+++ day1/part2.nim
@@ -0,0 +1,68 @@
+import std/strutils
+
+const
+    WINDOW_SIZE = 3
+
+# WTF no ++ operator in nim?
+proc increment(x: ptr int): int =
+  x[] += 1
+  x[]
+
+proc calculate(inputSeq: seq[string]): seq[int] =
+    # calculate inputSequence
+    var resPos = 0 # position of result array
+    var winPos = 0 
+    var line: string
+    var res = newSeq[int](1)
+    var i: int
+    while i < inputSeq.len:
+        line = inputSeq[i]
+        if isEmptyOrWhitespace(line):
+            break
+#        echo line, " ", $i
+#        echo $res[resPos], " + ", $parseInt(line), " = ", $(res[resPos] + parseInt(line))
+        res[resPos] = res[resPos] + parseInt(line)
+        inc winPos
+ 
+        if winPos == WINDOW_SIZE and i != inputSeq.len - (WINDOW_SIZE - 1):
+            inc(resPos)
+            res.add(0)
+            winPos = 0
+            dec(i) # move window one step back
+        else:
+            inc(i)
+
+    res
+
+
+proc solve() =
+    let f = open("input.txt")
+    defer: f.close()
+
+    # read complete file
+    var 
+        input = readAll(f)
+        inputSeq = input.split('\n')
+        res = calculate(inputSeq)
+        lastDepth = 0
+        count = 0
+    
+    for curDepth in res.items:
+        var status = $count & "(N/A - no previous measurement)"
+
+        if lastDepth > 0:
+            status = (  if curDepth > lastDepth: 
+                            $increment(addr count) & "(increased)" 
+                        elif curDepth == lastDepth:
+                            $count & "(no change)"
+                        else: 
+                            $count & "(decreased)"
+                     )
+        
+        echo $curDepth, ": ", status
+        lastDepth = curDepth
+
+    echo "Total: ", $count
+
+solve()
+
